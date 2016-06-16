6d889fbc8d62983c653199bd50f2d9db9ec8d095
prabhnith <prabhjot.nith@gmail.com>
2016-06-16 16:47:06 +0530

    Initial working commit

---
+++ index.android.js
@@ -0,0 +1,119 @@
+import React, {
+  Component,
+} from 'react';
+import {
+  AppRegistry,
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+class MoviesApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie.bind(this)}
+        style={styles.listView}
+      />
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e4F5FA',
+    borderWidth: 0.5,
+    borderRadius: 5,
+    padding:5,
+    marginBottom:5,
+
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+  	fontWeight: 'bold',
+    fontSize: 15,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 60,
+    height: 90,
+    borderWidth: 5,
+  },
+  listView: {
+    paddingTop: 50,
+    backgroundColor: '#F5FCFF',
+    padding: 10
+  },
+});
+
+AppRegistry.registerComponent('MoviesApp', () => MoviesApp);
